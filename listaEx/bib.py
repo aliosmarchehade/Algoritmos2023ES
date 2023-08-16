d87b132c0e32931867649eb8f4731e51f0bde42e
Ali Osmar <63081921+aliosmarchehade@users.noreply.github.com>
2023-08-15 22:45:53 -0300

    Add files via upload
    Exercicios sobre função da aula 15/08

---
+++ listaEx/bib.py
@@ -0,0 +1,70 @@
+import random
+
+def pedirNum():
+    a = int(input("Informe um número inteiro: "))
+    return a
+
+def exibirMenu():
+    print(f"Selecione uma das opções de exercício: 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ou 19: ")
+   
+   
+def gerarAleatorios(num):
+    b = random.randint(1, num)
+    return b
+
+def gerarPar(min,max):
+    b = random.randint(min, max)
+    while (b % 2 != 0):
+        b = random.randint(min, max)
+    return b
+
+def gerarImpar(min,max):
+    b = random.randint(min, max)
+    while (b % 2 == 0):
+        b = random.randint(min, max)
+    return b
+
+def mes(num):
+    if (num == 1):
+        return "Janeiro"
+    elif (num == 2):
+        return "Fevereiro"
+    elif (num == 3):
+        return "Março"
+    elif (num == 4):
+        return "Abril"
+    elif (num == 5):
+        return "Maio"
+    elif (num == 6):
+        return "Junho"
+    elif (num == 7):
+        return "Julho"
+    elif (num == 8):
+        return "Agosto"
+    elif (num == 9):
+        return "Setembro"
+    elif (num == 10):
+        return "Outubro"
+    elif (num == 11):
+        return "Novembro"
+    elif (num == 12):
+        return "Dezembro"
+    else:
+        return "Mês Inválido!"
+    
+def quadrado(x):
+   
+    return x**2
+
+def retangulo(base, altura):
+
+    return base * altura
+
+def triangulo (base, altura):
+
+    return (base * altura) / 2
+
+def trapezio (baseMaior, baseMenor, altura):
+
+    return ((baseMaior + baseMenor) * altura) / 2
+    
---
+++ listaEx/main.py
@@ -0,0 +1,57 @@
+import bib, random
+
+num =  bib.pedirNum()
+print(f"O número informado foi {num}")
+
+
+
+while(True):
+    bib.exibirMenu()
+    op = int(input("Selecione uma opção: "))
+
+
+    if(op == 3):
+        num = bib.pedirNum()
+        a = bib.gerarAleatorios(num)
+        print(f"O número aleatório gerado foi: {a}")
+
+    if(op == 4):
+        min = bib.pedirNum()
+        max = bib.pedirNum()
+        par = bib.gerarPar(min,max)
+        impar = bib.gerarImpar(min,max)
+        print(f"O numero par aleatório gerado foi o: {par}")
+        print(f"O numero ímpar aleatório gerado foi o: {impar}")
+    
+    if(op == 5):
+        num = bib.pedirNum()
+        mes = bib.mes(num)
+        print(f"O mês correspondente é: {mes}")
+    
+    if(op == 6):
+        print("** SELECIONE A EQUAÇÃO DESEJADA: \n A. B. C. ou D.: ")
+        op = input("Selecione uma opção: ")
+
+        if(op == "A" or op == "a"):
+            x = bib.pedirNum()
+            quad = bib.quadrado(x)
+            print(f"O quadrado do número {x} é {quad}")
+        
+        elif(op == "B" or op == "b"):
+            altura = bib.pedirNum()
+            base = bib.pedirNum()
+            ret = bib.retangulo(base, altura)
+            print(f"A área do retângulo é {ret}")
+        
+        elif(op == "C" or op == "c"):
+            altura = bib.pedirNum()
+            base = bib.pedirNum()
+            tra = bib.triangulo(base, altura)
+            print(f"A área do triângulo é {tra}")
+        
+        elif (op == "D" or op == "d"):
+            altura = bib.pedirNum()
+            baseMaior = bib.pedirNum()
+            baseMenor = bib.pedirNum()
+            trap = bib.trapezio(baseMaior, baseMenor, altura)
+            print(f"A área do trapézio é {trap}")
