a3a239338aa8347a7ca4812b089e82163cf4dd15
Ali Osmar <63081921+aliosmarchehade@users.noreply.github.com>
2023-09-21 13:51:56 -0300

    Update bib.py

--- 1909/bib.py
+++ 1909/bib.py
@@ -14,12 +14,10 @@ def inserirDados(nome):
     arquivo.write(nome + ",")
 
     email = input("Informe seu e-mail: ")
-    arquivo.write(email)
-    arquivo.write(",")
+    arquivo.write(email + ",")
 
     salario = input("Informe seu salário: ")
-    arquivo.write(salario)
-    arquivo.write(",")
+    arquivo.write(salario + ",")
 
     dtnasc = input("Informe sua data de nascimento: ")
     arquivo.write(dtnasc)
@@ -38,12 +36,13 @@ def listarDados(nome):
 
         dados = linha[i].replace("\n", "")
         dados = dados.split(",")
-        print(f"Nome: {dados[0]}")
-        print(f"Email: {dados[1]}")
-        print(f"Salário: {dados[2]}")
-        print(f"Data de nascimento: {dados[3]}")
-
-        print(linha[i].replace("\n" , "")) 
+        if(len(dados) >=4):
+            print(f"Nome: {dados[0]}")
+            print(f"Email: {dados[1]}")
+            print(f"Salário: {dados[2]}")
+            print(f"Data de nascimento: {dados[3]}")
+            print()  # Separar cada registro com uma linha em branco
+        
 
     arquivo.close()
 
@@ -52,9 +51,10 @@ def buscarDados(nome, procurar):
     linha = arquivo.readlines()
     for i in range(0, len(linha)):
         dados = linha[i].split(";")
-        if(procurar in dados[0]):
-            print(f"O nome {procurar} está listado!")
-
+    if(procurar in dados[0]):
+        print(f"O nome {procurar} está listado!")
+    else:
+        print(f"Este nome não está na base de dados!")
     arquivo.close()
 
 def somaSalarios(nome):
@@ -62,7 +62,10 @@ def somaSalarios(nome):
     linha = arquivo.readlines()
     salario = 0
     for i in range(0, len(linha)):
-        dados = linha[i].split(";")
-        dados[2] = int(dados[2])
-        salario += dados[2]
+        dados = linha[i].split(",")
+        if(len(dados) >=4):
+            dados[2] = int(dados[2])
+            salario += dados[2]
+
+    print(f"A soma de todos os salários foi de: {salario}")
     arquivo.close()
