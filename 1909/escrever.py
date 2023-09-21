d6f63c098edf11507b442b3c92e31b6e6efac97b
Ali Osmar <63081921+aliosmarchehade@users.noreply.github.com>
2023-09-21 13:53:36 -0300

    Update escrever.py

--- 1909/escrever.py
+++ 1909/escrever.py
@@ -1,22 +1,22 @@
 
-arquivo = open("nomes.txt", "a")
+# arquivo = open("nomes.txt", "a")
 
 
-nome = input("Informe seu nome completo: ")
-arquivo.write(nome + ",")
+# nome = input("Informe seu nome completo: ")
+# arquivo.write(nome + ",")
 
-email = input("Informe seu e-mail: ")
-arquivo.write(email)
-arquivo.write(",")
+# email = input("Informe seu e-mail: ")
+# arquivo.write(email)
+# arquivo.write(",")
 
-salario = input("Informe seu salário: ")
-arquivo.write(salario)
-arquivo.write(",")
+# salario = input("Informe seu salário: ")
+# arquivo.write(salario)
+# arquivo.write(",")
 
-dtnasc = input("Informe sua data de nascimento: ")
-arquivo.write(dtnasc)
+# dtnasc = input("Informe sua data de nascimento: ")
+# arquivo.write(dtnasc)
 
 
-arquivo.write("\n")
+# arquivo.write("\n")
 
-arquivo.close()
+# arquivo.close()
